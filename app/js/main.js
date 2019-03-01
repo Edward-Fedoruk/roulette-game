b296203bd3913fbed51fba054db41fcfd9ddbf69
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-01 23:39:52 +0200

    lines render function for the reels

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,32 @@
   }
   
   setInterval(clock, 1000)
+})();
+
+
+(function() {
+  const response = JSON.parse(serverResponse)
+
+  const test = new Array(56).fill(0)
+
+  const reels = document.querySelector(".reel__desk-leftReal")
+  const reels2 = document.querySelector(".reel__desk-rightReal")
+  const lineRotateDegree = 360 / test.length   
+
+  // fix for X position change when items are less then 30 
+  const leftOffset = test.length < 36 ? (47 - (30 - test.length) * 0.5) : 47  
+  
+  const tessst = (testCase, i) => {
+    const testElem = document.createElement("div")
+    testElem.classList.add("reel__line")
+    testElem.style.transform = `rotate(${lineRotateDegree * i}deg)`
+    testElem.style.borderRightWidth = `${lineRotateDegree / 10}vw`
+    testElem.style.borderLeftWidth = `${lineRotateDegree / 10}vw`
+    testElem.style.left = `${leftOffset}%`
+    reels.appendChild(testElem)  
+  }
+
+  test.forEach(tessst)
+  reels2.forEach(tessst)
+  
 })()
