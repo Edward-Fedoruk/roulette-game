302734420a78f1d3d82d6c9af963dce79a664c81
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-02 17:26:53 +0200

    render dashboard info from response

---
+++ app/js/renderDashboard.js
@@ -0,0 +1,31 @@
+(function() {
+
+  const { history } = JSON.parse(serverResponse)
+
+  const dashboard = document.querySelector(".dashboard__info")
+
+  console.log(history, dashboard)
+
+  const renderRows = row => {
+    const { id, prize, computer, created_at } = row
+
+    const date = new Date(created_at)
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+
+    const dashboardRow = document.createElement("div")
+    dashboardRow.classList.add("dashboard__row")
+    dashboardRow.innerHTML = `
+      <span class="dashboard__spinData">${id}</span>
+      <span class="dashboard__spinData">${hours}:${minutes}</span>
+      <span class="dashboard__spinData">${prize}</span>
+      <span class="dashboard__spinData">${computer}</span>
+    `
+    return dashboardRow
+  }
+
+  const dashboardRows = history.map(renderRows)
+  dashboard.append(...dashboardRows)
+  
+
+})()
