40775f7edc69d07c6e19dbc16b53dc94e482c893
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-03 16:38:55 +0200

    spin for right reel and calculating winning triangle

--- app/js/spinReels.js
+++ app/js/spinReels.js
@@ -1,18 +1,56 @@
 (function() {
   
   let { 
-    club_info: { repeats_interval, count_repeats } 
+    club_info: { repeats_interval, count_repeats, computers },
+    prizes 
   } = JSON.parse(SERVER_RESPONSE)
  
+  const spinResults = {
+    id: 0,
+    time: "00:00",
+    userPrize: 500,
+    computerPrize: 20
+  }
+
+  const computeWinningIndex = (randomDegree, trianglesLength, accuracy = 0) => {
+    const passedDegree = randomDegree % 360 // diff between 360* and the end position 
+    const triangleDegreeStep = 360 / trianglesLength
+    const quarterOfTriangles = Math.floor(trianglesLength / 4)
+    const winningIndex = Math.floor(passedDegree / triangleDegreeStep) + quarterOfTriangles + accuracy
+
+    if(winningIndex >= computers.length) winningIndex = winningIndex % computers.length
+
+    return winningIndex
+  }
+
+
   const spinReel = () => {
     const reel = document.querySelector(".reel__desk-leftReal")
-    const randomDegree = Math.round(Math.random() * 360) + 360
+    const randomDegree = Math.floor(Math.random() * 360) + 360
+    
+    const winningIndex = computeWinningIndex(randomDegree, prizes.length)
+
+    const winningTriangle = [...reel.children].reverse()[winningIndex] 
+
+    console.log(winningTriangle, winningIndex)
 
     reel.style.transform = `rotate(${randomDegree}deg)`
-
-
   }
+
+  const spinArrow = () => {
+    const arrow = document.querySelector(".reel__arrow-rightReel")
+    const randomDegree = Math.floor(Math.random() * 360) + 360
+    const triangles = document.querySelector(".reel__desk-rightReal").children
+
+    const winningIndex = computeWinningIndex(randomDegree, computers.length, 2)
+
+    console.log([...triangles][winningIndex], winningIndex)
+
+    arrow.style.transform = `rotate(${randomDegree}deg)`
+  }
+
   spinReel()
+  spinArrow()
   
   // setInterval(spinReel, repeats_interval)
 
