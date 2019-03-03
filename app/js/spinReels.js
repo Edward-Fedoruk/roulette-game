f331286e1295e79d9d8a5ec250c89d71386f0f1a
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-03 18:44:28 +0200

    render results to the table + small fixes

--- app/js/spinReels.js
+++ app/js/spinReels.js
@@ -1,57 +1,80 @@
 (function() {
   
   let { 
-    club_info: { repeats_interval, count_repeats, computers },
-    prizes 
+    club_info: { repeats_interval, computers },
+    prizes,
+    history,
   } = JSON.parse(SERVER_RESPONSE)
  
   const spinResults = {
-    id: 0,
+    id: history[history.length - 1].id,
     time: "00:00",
-    userPrize: 500,
-    computerPrize: 20
+    userPrize: "500",
+    computerPrize: "20"
   }
 
   const computeWinningIndex = (randomDegree, trianglesLength, accuracy = 0) => {
     const passedDegree = randomDegree % 360 // diff between 360* and the end position 
     const triangleDegreeStep = 360 / trianglesLength
     const quarterOfTriangles = Math.floor(trianglesLength / 4)
-    const winningIndex = Math.floor(passedDegree / triangleDegreeStep) + quarterOfTriangles + accuracy
+    let winningIndex = Math.floor(passedDegree / triangleDegreeStep) + quarterOfTriangles + accuracy
 
-    if(winningIndex >= computers.length) winningIndex = winningIndex % computers.length
+    while(winningIndex >= trianglesLength ) {
+      winningIndex = winningIndex % trianglesLength
+      if(winningIndex === trianglesLength) break
+    }
 
     return winningIndex
   }
 
+  const setRandomDegree = () => Math.floor(Math.random() * 360) + 360
+
+  const setSpinResults = prize => {
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    spinResults.id++
+    spinResults.time = `${hours}:${minutes}`
+    spinResults.userPrize = `${prize}`
+  }
+
+  const appendRow = () => {
+    const { id, time, userPrize, computerPrize } = spinResults
+    const dashboardRow = document.createElement("div")
+    dashboardRow.classList.add("dashboard__row")
+    dashboardRow.innerHTML = `
+      <span class="dashboard__spinData">${id}</span>
+      <span class="dashboard__spinData">${time}</span>
+      <span class="dashboard__spinData">${userPrize}</span>
+      <span class="dashboard__spinData">${computerPrize}</span>
+    `
+    document.querySelector(".dashboard__info").append(dashboardRow)
+  }
 
   const spinReel = () => {
     const reel = document.querySelector(".reel__desk-leftReal")
-    const randomDegree = Math.floor(Math.random() * 360) + 360
-    
+    const randomDegree = setRandomDegree()
     const winningIndex = computeWinningIndex(randomDegree, prizes.length)
-
-    const winningTriangle = [...reel.children].reverse()[winningIndex] 
-
-    console.log(winningTriangle, winningIndex)
+    const winningTriangle = [...reel.children].reverse()[winningIndex]
+    
+    setSpinResults(winningTriangle.children[0].textContent)
 
     reel.style.transform = `rotate(${randomDegree}deg)`
   }
 
   const spinArrow = () => {
     const arrow = document.querySelector(".reel__arrow-rightReel")
-    const randomDegree = Math.floor(Math.random() * 360) + 360
+    const randomDegree = setRandomDegree()  
     const triangles = document.querySelector(".reel__desk-rightReal").children
-
     const winningIndex = computeWinningIndex(randomDegree, computers.length, 2)
+    const winningTriangle = [...triangles][winningIndex]
 
-    console.log([...triangles][winningIndex], winningIndex)
-
+    spinResults.computerPrize = winningTriangle.children[0].textContent
     arrow.style.transform = `rotate(${randomDegree}deg)`
   }
 
-  spinReel()
-  spinArrow()
-  
-  // setInterval(spinReel, repeats_interval)
-
+    
+  setInterval(spinReel, repeats_interval)
+  setInterval(spinArrow, repeats_interval + 3000)
+  setInterval(appendRow, repeats_interval + 3000)
 })()
